37ba0244fd3df115fd1144e1693d2649e50b32da
Jack Weeden <jack@ajack.org>
2014-09-15 12:15:58 +0100

    Allow images to be resized/repositioned Execute function
    Selector for image size
    
    Added position dropdown
    
    Label for dropdown
    
    Build dropdowns
    
    Add log
    
    Fix rendering object
    
    Fix rendering object
    
    Add debugger
    
    Add debugger
    
    Debugging toData
    
    Don't overwrite entire editor with image
    
    Don't overwrite entire editor with image
    
    Set image size/position data
    
    Use correct attr
    
    Use CSS position spelling
    
    Set dropdowns when editing
    
    Fix
    
    Debugger
    
    Fix
    
    Fix
    
    Fix
    
    Fix
    
    Fix
    
    Fix
    
    Fix
    
    Fix
    
    Fix
    
    Fix
    
    Fix
    
    Fix
    
    Fix
    
    Fix
    
    Fix
    
    Fix
    
    Image positions/sizes

--- src/blocks/image.js
+++ src/blocks/image.js
@@ -5,7 +5,13 @@
 SirTrevor.Blocks.Image = (function() {
 
   var template = _.template([
-    '<p>HELLO!</p>'
+    '<div class="image-size">',
+      '<select>',
+        '<option name="small">Small</option>',
+        '<option name="medium">Medium</option>',
+        '<option name="large">Large</option>',
+      '</select>',
+    '</div>'
   ].join("\n"));
 
 
@@ -18,14 +24,19 @@ SirTrevor.Blocks.Image = (function() {
     uploadable: true,
 
     editorHTML: function() {
-      return template(this);
+      return this.buildDropdowns();
     },
 
     icon_name: 'image',
 
     loadData: function(data){
-      // Create our image tag
-      this.$editor.html($('<img>', { src: data.file.url }));
+      var img = $('<img>', { src: data.file.url });
+      this.$editor.append(img);
+
+      this.findInputs();
+      this.sizeSelect.val(data.size);
+      this.positionSelect.val(data.position);
+      this.updateImage();
     },
 
     onBlockRender: function(){
@@ -34,6 +45,9 @@ SirTrevor.Blocks.Image = (function() {
       this.$inputs.find('input').on('change', _.bind(function(ev){
         this.onDrop(ev.currentTarget);
       }, this));
+
+      this.$(".image-options").show();
+      this.bindSelectListeners();
     },
 
     onUploadSuccess : function(data) {
@@ -55,11 +69,93 @@ SirTrevor.Blocks.Image = (function() {
         this.loading();
         // Show this image on here
         this.$inputs.hide();
-        this.$editor.html($('<img>', { src: urlAPI.createObjectURL(file) })).show();
+        this.$editor.append($('<img>', { src: urlAPI.createObjectURL(file) })).show();
 
         this.uploader(file, this.onUploadSuccess, this.onUploadError);
       }
+    },
+
+    toData: function() {
+      this.findInputs();
+      var imageData = this.getData();
+      var imageSize = this.sizeSelect.val();
+      var imagePos  = this.positionSelect.val();
+      imageData["size"]     = imageSize;
+      imageData["position"] = imagePos;
+      this.setData(imageData);
+    },
+
+    buildDropdowns: function() {
+      return [
+        '<div class="image-options">',
+          this.buildSizeDropdown(),
+          this.buildPositionDropdown(),
+        '</div>'
+      ].join("\n");
+    },
+
+    buildSizeDropdown: function() {
+      return [
+        '<span>Size:</span>',
+        '<select class="image-size">',
+          '<option value="small">Small</option>',
+          '<option value="medium">Medium</option>',
+          '<option value="large">Large</option>',
+        '</select>',
+      ].join("\n")
+    },
+
+    buildPositionDropdown: function() {
+      return [
+        '<span>Position:</span>',
+        '<select class="image-position" style="margin-bottom: 12px;">',
+          '<option value="left">Left</option>',
+          '<option value="center">Centre</option>',
+          '<option value="right">Right</option>',
+        '</select>',
+      ].join("\n")
+    },
+
+    bindSelectListeners: function() {
+      this.findInputs();
+      var self = this;
+
+      this.sizeSelect.on("change", function() {
+        self.updateImage();
+      });
+
+      this.positionSelect.on("change", function() {
+        self.updateImage();
+      });
+    },
+
+    updateImage: function(options) {
+      this.findInputs();
+      var self = this;
+
+      var css = {float: "none", margin: "0", display: "inherit"};
+
+      var width = this.sizeSelect.val();
+      if (width == "small")  { css.width = "50%"; }
+      if (width == "medium") { css.width = "75%"; }
+      if (width == "large")  { css.width = "100%"; }
+
+      var position = this.positionSelect.val();
+      if (position == "center") {
+        css.margin = "0 auto";
+        css.display = "block";
+      } else if (position == "right") {
+        css.float = "right";
+        this.$el.find("img").after('<div style="clear:both" class="clear-both-image"></div>');
+      }
+
+      this.$el.find("img").css(css);
+    },
+
+    findInputs: function() {
+      this.sizeSelect      = this.$el.find("select.image-size");
+      this.positionSelect  = this.$el.find("select.image-position");
     }
   });
 
-});
+})();
